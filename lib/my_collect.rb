b8bcfa41d9bc20ebeca40a8ccaaa50b96d757e46
Megan Fago <meganlaynefago@gmail.com>
2019-01-17 23:27:37 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,20 +1,9 @@
-# student = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-# collection = ["ruby", "javascript", "python", "objective-c"]
-
 def my_collect (array)
  new_array = []
   i = 0
   while i < array.length
   new_array << yield(array[i])
     i = i+1
-
 end
   new_array
 end
-
-# my_collect(student) do |name|
-#   name.split(" ").first
-# end
-# my_collect(collection) do |lang|
-#   lang.upcase
-# end
