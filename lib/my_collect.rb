82c586f9b857092940402c1b85f56e5581ce7c13
Megan Fago <meganlaynefago@gmail.com>
2019-01-17 23:07:20 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+# student = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+# collection = ["ruby", "javascript", "python", "objective-c"]
 
-
+ def my_collect(array)
+  new_arr = [] 
+  
+   i=0 
+   while i < array.length 
+   new_arr.push yield(array[i])
+   i += 1 
+ end 
